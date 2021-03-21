fbd96c3fd07018d82ad3bb9cb6ededf56fd2f6d2
Justin Xavier <justin.deimos@gmail.com>
2021-03-21 21:02:11 +0530

    Updated doctor admin and frontend for doctor

--- epatientbackend/app/config/env.js
+++ epatientbackend/app/config/env.js
@@ -1,8 +1,8 @@
 const env = {
-  database: 'justindb',
-  username: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
+  database: 'xsguchft',
+  username: 'xsguchft',
+  password: 'Nu0FM41OkheedK61hkZVrYvfqWS-rr2a',
+  host: 'queenie.db.elephantsql.com',
   dialect: 'postgres',
   pool: {
     max: 5,
--- epatientfrontend/src/app/addoc/addoc.component.ts
+++ epatientfrontend/src/app/addoc/addoc.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { Doctor } from '../doctor';
+import { DoctorService } from '../doctor.service';
+import { Message } from '../message';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-addoc',
   templateUrl: './addoc.component.html',
@@ -7,9 +12,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddocComponent implements OnInit {
 
-  constructor() { }
+  doctor: Doctor;
+  /**
+   * Constructing Http Patient Service
+   * @param doctorService 
+   */
+
+
+  constructor(private doctorService: DoctorService,
+    private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.doctor = new Doctor();
   }
+
+/**
+   * Store a Doctor to backend server
+   */
+ save() {
+  this.doctorService.createDoctor(this.doctor)
+        .subscribe((message: Message) => {
+          console.log(message);
+          let doctor = message.doctors[0];
+          let msg = "Success -> Post a Doctor: " 
+              + "<ul>"
+                  + "<li>id: " + doctor.id + "</li>"  
+                  + "<li>firstname: " + doctor.firstname + "</li>"
+                  + "<li>lastname: " + doctor.lastname + "</li>"
+                  + "<li>age: " + doctor.age + "</li>"
+                  + "<li>address: " + doctor.email + "</li>"
+              + "</ul>";
+
+          this.messageService.add(msg);
+        }, error => {
+          console.log(error);
+          let msg = "Error! -> Action Posting a Doctor:" 
+                    + "<ul>"
+                      + "<li>id = " + this.doctor.id + "</li>"  
+                      + "<li>firstname = " + this.doctor.firstname + "</li>"
+                      + "<li>lastname = " + this.doctor.lastname + "</li>"
+                      + "<li>age = " + this.doctor.age + "</li>"
+                      + "<li>address = " + this.doctor.email + "</li>"
+                    + "</ul>";
+
+          this.messageService.add(msg);
+        });
+}
+
+reset(){
+  this.doctor = new Doctor();
+}
+
+/**
+   * Function handles form submitting
+   */
+ onSubmit() {
+  this.save();
+  this.reset();
+}
 
 }
